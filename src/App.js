ce21ef7bb818e148fd959c9efb158561a4e3fe7e
NirjharJoshi <njupkiet@gmail.com>
2023-01-25 11:41:00 +0530

    added favicon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="d-flex flex-column align-items-center border border-2 border-primary mx-auto mt-5"
+      style={{ width: "500px", height: "600px" }}
+    >
+      <i
+        className="fa-solid fa-indian-rupee-sign text-primary mt-3 mb-5 border border-2 border-secondary"
+        style={{ fontSize: "80px" }}
+      ></i>
     </div>
   );
 }
